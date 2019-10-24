f92d411f571df0d07a5287e34857a9cc8e7a47a7
iLay1678 <807190135@qq.com>
2019-10-24 11:08:20 +0800

    返回值完善

--- server.php
+++ server.php
@@ -184,6 +184,9 @@ $app->server->push(function ($message) {
                                     case "1":
                                         return "biubiubiu~发送成功";
                                         break;
+									case "-2":
+                                        return "信息缺失";
+                                        break;
                                     case "-3":
                                         return "身份验证失败";
                                         break;
